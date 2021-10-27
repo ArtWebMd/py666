3c658c7ed4874296ae7dabf44852f56a8993e527
ArtWebMd <93036114+ArtWebMd@users.noreply.github.com>
2021-10-27 21:16:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,53 +29,69 @@ def verify_competitors():
         user_ad = requests.get(full_link)
         parse_page = BeautifulSoup(user_ad.content, 'html.parser')
         user_names = parse_page.select('a.adPage__aside__stats__owner__login')
-        for u_name in user_names:
-            user_names_array.append(u_name.contents[0].strip())
-            print(u_name.contents[0].strip())
+        for i in range(len(user_names)):
+            print(user_names[i])
+            if(user_names[0].strip() == 'rentcalianmd'):
+                time.sleep(10)
+                verify_competitors()
+                print('Rentcalian e nambar oan')
+            else:
+                if(user_names[i] == 'vinzarionline' || user_names[i] == 'aaandreiii' || user_names[i] == 'pislaru-888' || user_names[i] == 'rentinthecapital' || user_names[i] == 'smoke-lab-md'):
+                    if(user_names[i-1] != 'rentcalianmd'):
+                        kill_competitors()
+                    else:
+                        time.sleep(10)
+                else:
+                    time.sleep(10)
+                    verify_competitors()
+                    print('Verificam din nou')
+#         for u_name in user_names:
+#             user_names_array.append(u_name.contents[0].strip())
+#             print(u_name.contents[0].strip())
         # print(user_names)
     # print(divs)
-    if('rentcalianmd' in user_names_array):
-        rentcalian_index = user_names_array.index('rentcalianmd')
-    else:
-        rentcalian_index = 9999
+#     if('rentcalianmd' in user_names_array):
+#         rentcalian_index = user_names_array.index('rentcalianmd')
+#     else:
+#         rentcalian_index = 9999
 
-    if('vinzarionline' in user_names_array):
-        vinzarionline_index = user_names_array.index('vinzarionline')
-    else:
-        vinzarionline_index = 9999
+#     if('vinzarionline' in user_names_array):
+#         vinzarionline_index = user_names_array.index('vinzarionline')
+#     else:
+#         vinzarionline_index = 9999
 
-    if('aaandreiii' in user_names_array):
-        aaandreiii_index = user_names_array.index('aaandreiii')
-    else:
-        aaandreiii_index = 9999
+#     if('aaandreiii' in user_names_array):
+#         aaandreiii_index = user_names_array.index('aaandreiii')
+#     else:
+#         aaandreiii_index = 9999
 
-    if('pislaru-888' in user_names_array):
-        pislaru888_index = user_names_array.index('pislaru-888')
-    else:
-        pislaru888_index = 9999
+#     if('pislaru-888' in user_names_array):
+#         pislaru888_index = user_names_array.index('pislaru-888')
+#     else:
+#         pislaru888_index = 9999
 
-    if('rentinthecapital' in user_names_array):
-        rentinthecapital_index = user_names_array.index('rentinthecapital')
-    else:
-        rentinthecapital_index = 9999
+#     if('rentinthecapital' in user_names_array):
+#         rentinthecapital_index = user_names_array.index('rentinthecapital')
+#     else:
+#         rentinthecapital_index = 9999
 
-    if('smoke-lab-md' in user_names_array):
-        smoke_lab_md_index = user_names_array.index('smoke-lab-md')
-    else:
-        smoke_lab_md_index = 9999
+#     if('smoke-lab-md' in user_names_array):
+#         smoke_lab_md_index = user_names_array.index('smoke-lab-md')
+#     else:
+#         smoke_lab_md_index = 9999
 
-    if(rentcalian_index > vinzarionline_index or rentcalian_index > aaandreiii_index or rentcalian_index > pislaru888_index or rentcalian_index > rentinthecapital_index or rentcalian_index > smoke_lab_md_index):
-        # chemam functia republicare
-        print('Republicam acum, procesul de ucidere a concurentilor incepe:')
-        time.sleep(5)
-        kill_competitors()
-    else:
-        # repetam peste 1 minut
-        time.sleep(5)
-        print('Verificam din nou concurentii')
-        verify_competitors()
+#     if(rentcalian_index > vinzarionline_index or rentcalian_index > aaandreiii_index or rentcalian_index > pislaru888_index or rentcalian_index > rentinthecapital_index or rentcalian_index > smoke_lab_md_index):
+#         # chemam functia republicare
+#         print('Republicam acum, procesul de ucidere a concurentilor incepe:')
+#         time.sleep(5)
+#         kill_competitors()
+#     else:
+#         # repetam peste 1 minut
+#         time.sleep(5)
+#         print('Verificam din nou concurentii')
+#         verify_competitors()
 
-    print(str(rentcalian_index) + '\n' + str(vinzarionline_index) + '\n' + str(aaandreiii_index) + '\n' + str(pislaru888_index) + '\n' + str(rentinthecapital_index) + '\n' + str(smoke_lab_md_index))
+#     print(str(rentcalian_index) + '\n' + str(vinzarionline_index) + '\n' + str(aaandreiii_index) + '\n' + str(pislaru888_index) + '\n' + str(rentinthecapital_index) + '\n' + str(smoke_lab_md_index))
 
 def kill_competitors():
     driver = webdriver.Chrome()
