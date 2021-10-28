9d999183c2557361f881c9c72c4851d80bebe4c0
ArtWebMd <93036114+ArtWebMd@users.noreply.github.com>
2021-10-28 21:59:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options = chrome_options)
 
 
+
 def verify_competitors():
     page = requests.get("https://999.md/ru/list/all-for-celebrations/gifts-for-fans-of-smoking")
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -30,68 +31,27 @@ def verify_competitors():
         parse_page = BeautifulSoup(user_ad.content, 'html.parser')
         user_names = parse_page.select('a.adPage__aside__stats__owner__login')
         for i in range(len(user_names)):
-            print(user_names[i].contents[0])
+            print(user_names[i].contents[0].strip())
             if(user_names[0].contents[0].strip() == 'rentcalianmd'):
+                print('RentCalian Nambar oan!!! Incepem de la inceput curand!')
                 time.sleep(10)
                 verify_competitors()
-                print('Rentcalian e nambar oan')
             else:
-                if(user_names[i].contents[0] == 'vinzarionline' or user_names[i].contents[0] == 'aaandreiii' or user_names[i].contents[0] == 'pislaru-888' or user_names[i].contents[0] == 'rentinthecapital' or user_names[i].contents[0] == 'smoke-lab-md'):
-                    if(user_names[i-1].contents[0] != 'rentcalianmd'):
-                        kill_competitors()
-                    else:
+                if(user_names[i].contents[0].strip() == 'vinzarionline' or user_names[i].contents[0].strip() == 'aaandreiii' or user_names[i].contents[0].strip() == 'pislaru-888' or user_names[i].contents[0].strip() == 'rentinthecapital' or user_names[i].contents[0].strip() == 'smoke-lab-md'):
+                    if(user_names[i-1].contents[0] == 'rentcalianmd'):
+                        print('RentCalian Nambar oan, verificam din nou')
                         time.sleep(10)
-                else:
-                    time.sleep(10)
-                    print('Verificam din nou')
-#         for u_name in user_names:
-#             user_names_array.append(u_name.contents[0].strip())
-#             print(u_name.contents[0].strip())
+                        verify_competitors()
+                    else:
+                        print('Ucidem concurentii start!')
+                        kill_competitors()
+            # user_names_array.append(u_name.contents[0].strip())
+            # print(u_name.contents[0].strip())
         # print(user_names)
     # print(divs)
-#     if('rentcalianmd' in user_names_array):
-#         rentcalian_index = user_names_array.index('rentcalianmd')
-#     else:
-#         rentcalian_index = 9999
+    
 
-#     if('vinzarionline' in user_names_array):
-#         vinzarionline_index = user_names_array.index('vinzarionline')
-#     else:
-#         vinzarionline_index = 9999
-
-#     if('aaandreiii' in user_names_array):
-#         aaandreiii_index = user_names_array.index('aaandreiii')
-#     else:
-#         aaandreiii_index = 9999
-
-#     if('pislaru-888' in user_names_array):
-#         pislaru888_index = user_names_array.index('pislaru-888')
-#     else:
-#         pislaru888_index = 9999
-
-#     if('rentinthecapital' in user_names_array):
-#         rentinthecapital_index = user_names_array.index('rentinthecapital')
-#     else:
-#         rentinthecapital_index = 9999
-
-#     if('smoke-lab-md' in user_names_array):
-#         smoke_lab_md_index = user_names_array.index('smoke-lab-md')
-#     else:
-#         smoke_lab_md_index = 9999
-
-#     if(rentcalian_index > vinzarionline_index or rentcalian_index > aaandreiii_index or rentcalian_index > pislaru888_index or rentcalian_index > rentinthecapital_index or rentcalian_index > smoke_lab_md_index):
-#         # chemam functia republicare
-#         print('Republicam acum, procesul de ucidere a concurentilor incepe:')
-#         time.sleep(5)
-#         kill_competitors()
-#     else:
-#         # repetam peste 1 minut
-#         time.sleep(5)
-#         print('Verificam din nou concurentii')
-#         verify_competitors()
-
-#     print(str(rentcalian_index) + '\n' + str(vinzarionline_index) + '\n' + str(aaandreiii_index) + '\n' + str(pislaru888_index) + '\n' + str(rentinthecapital_index) + '\n' + str(smoke_lab_md_index))
-
+    
 def kill_competitors():
     driver = webdriver.Chrome()
     driver.get("https://simpalsid.com/user/login?project_id=999a46c6-e6a6-11e1-a45f-28376188709b&lang=ru")
@@ -109,7 +69,7 @@ def kill_competitors():
 
     driver.get("https://999.md/cabinet/items/rentcalianmd?page=15&ad_states=public")
 
-    republish = driver.find_element_by_xpath('//*[@id="js-cabinet-items-list"]/tr[28]/td[7]/div/a')
+    republish = driver.find_element_by_xpath('/html/body/div[4]/div/section/div/div/div[1]/form/table/tbody/tr[28]/td[7]/div/a')
     republish.click()
 
     time.sleep(2)
@@ -123,7 +83,7 @@ def kill_competitors():
     # print(all_links)
     now = datetime.datetime.now()
     f = open("hours.txt", "a")
-    f.write(str(now.day) + '-' + str(now.hour) + ':' + str(now.minute) + '\n')
+    f.write(str(now.day) + '-' + str(now.hour) + ':' + str(now.minute))
     f.close()
     time.sleep(3)
     driver.close()
